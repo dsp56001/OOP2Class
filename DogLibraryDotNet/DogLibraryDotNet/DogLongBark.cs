e71cb249c8d335bcc816151cf3d005b4f87f6f97
Meyers <jmeyers@colum.edu>
2022-10-03 14:23:20 -0500

    update event demo add task

---
+++ DogLibraryDotNet/DogLibraryDotNet/DogLongBark.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DogLibrary;
+
+namespace DogLibraryDotNet
+{
+    public class DogLongBark : Dog
+    {
+
+
+
+        public override string Bark()
+        {
+            Console.WriteLine("Sleep for 5 seconds.");
+            Thread.Sleep(5000);
+
+            return base.Bark();
+        }
+
+        public async void BarkAsync()
+        {
+            await System.Threading.Tasks.Task.Delay(5000);
+        }
+
+
+
+        public async void BarkTask()
+        {
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine("taskA Status: {0}", taskA.Status);
+            try
+            {
+                taskA.Wait();
+                Console.WriteLine("taskA Status: {0}", taskA.Status);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Exception in taskA.");
+            }
+
+        }
+    }
+}
+
