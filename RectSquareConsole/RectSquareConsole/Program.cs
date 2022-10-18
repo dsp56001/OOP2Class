49d47e0d0401084d1d3cacf6b73ae4903cbd71e9
Jeff Meyers <jmeyers@colum.edu>
2022-10-18 13:42:58 -0500

    adds Rect Sqaure

---
+++ RectSquareConsole/RectSquareConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RectSquareConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //TODO Write some tests
+        }
+    }
+
+    class Rectangle
+    {
+        int Width, Height;
+
+        public int GetHeight()
+        {
+            return Height;
+        }
+        public virtual void SetHeight(int height)
+        {
+            this.Height = height;
+        }
+        public int GetWidth()
+        {
+            return this.Width;
+        }
+        public virtual void SetWidth(int width)
+        {
+            this.Width = width;
+        }
+
+        public bool IsSquare()
+        {
+            return this.Width == this.Height;
+        }
+
+    }
+
+    class Square : Rectangle
+    {
+        public override void SetWidth(int width)
+        {
+            this.SetHeight(width);
+            base.SetWidth(width);
+        }
+
+        public override void SetHeight(int height)
+        {
+            this.SetWidth(height);
+            base.SetHeight(height);
+        }
+    }
+}
